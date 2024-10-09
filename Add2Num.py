282e110f70169797ab2333b1a892166cd66f8b99
thanushaillaa <thanushailla5@gmail.com>
2024-10-09 19:54:51 +0530

    Add2Num

--- Add2Num.py
+++ Add2Num.py
@@ -1,5 +1,5 @@
-num1=10
-num2=15
+num1=int(input("enter num1 value: "))
+num2=int(input("enter num2 value: "))
 sum=num1+num2
 #using formatting output
 print('sum of {0} and {1} is {2}'. format(num1, num2, sum))
